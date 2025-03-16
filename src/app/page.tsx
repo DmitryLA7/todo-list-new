3d6c498b5ac584852fcba25c149f0bff7075e22c
DmitryLA7 <ambalnakacenni97@gmail.com>
2025-03-16 10:59:38 +0600

    feat: setting up global styles & fonts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,10 +5,13 @@ import { store } from "@/store";
 
 import { Page } from "@/components";
 
+import GlobalStyles from "./styles";
+
 export default function Home() {
   return (
     <Provider store={store}>
       <Page />
+      <GlobalStyles />
     </Provider>
   );
 }
---
+++ src/app/styles.ts
@@ -0,0 +1,23 @@
+import { createGlobalStyle } from "styled-components";
+
+
+export default createGlobalStyle`
+  
+
+    html,
+    body {
+        max-width: 100vw;
+    }
+
+    * {
+        box-sizing: border-box;
+        padding: 0;
+        margin: 0;
+    }
+
+    a {
+        color: inherit;
+        text-decoration: none;
+    }
+
+`
